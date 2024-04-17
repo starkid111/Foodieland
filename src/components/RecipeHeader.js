666bffd662ec9f15d740bbb5fc451a042278c7ad
Ramadan <Ramadanadex111@gmail.com>
2024-04-17 15:12:16 +0100

    Update RecipeHeader.js

--- src/components/RecipeHeader.js
+++ src/components/RecipeHeader.js
@@ -3,12 +3,22 @@ import { IoTimer } from "react-icons/io5";
 import { LuUtensils } from "react-icons/lu";
 import print from "../images/print.png";
 import share from "../images/share.png";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 const RecipeHeader = () => {
+
+
+    useEffect(() => {
+        AOS.init({duration:1000})
+    },[] )  
+
+
     return ( 
-        <div className="mt-24 lg:container lg:mx-auto">
-            <div className="flex flex-col space-y-5 md:space-y-0 md:flex-row  justify-between  p-5">
-                <div className="flex flex-col space-y-10">
+        <div className="mt-24 lg:container lg:mx-auto ">
+            <div className="flex flex-col space-y-5 md:space-y-0 md:flex-row  justify-between  p-5 overflow-hidden">
+                <div className="flex flex-col space-y-10 overflow-hidden" data-aos="fade-right">
                     <h1 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl font-medium font-sans'>Big and Juicy Wagyu Beef cheeseburger</h1>
                     <div className="flex flex-col space-y-5 md:items-center md:flex-row  space-x-3 lg:space-x-5">
 
@@ -49,7 +59,7 @@ const RecipeHeader = () => {
                    </div>
            
                 </div>
-                <div className="flex space-x-4 self-end md:self-auto">
+                <div className="flex space-x-4 self-end md:self-auto overflow-hidden" data-aos="fade-left">
                         <div className='flex flex-col space-y-2 items-center'>
                             <img src={print} alt=""  className='lg:w-full w-1/2'/>
                             <p>PRINT</p>
